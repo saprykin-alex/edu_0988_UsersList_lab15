7d233703551773b26aacd33391a4fd71ca597a0b
saprykin-alex <figulkin@gmail.com>
2021-09-02 02:46:47 +0300

    Лабораторная №14. Добавлены классы для работы с БД

---
+++ app/src/main/java/com/example/myappuserslist1/database/UserBaseHelper.java
@@ -0,0 +1,32 @@
+package com.example.myappuserslist1.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+//Класс для подключения к базе данных
+public class UserBaseHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "userBase.db";
+
+    public UserBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+    //Если БД не существует, то она будет создана
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table "+UserDbSchema.UserTable.NAME+" (" +
+                "_id integer primary key autoincrement, " +
+                UserDbSchema.Cols.UUID+", "+
+                UserDbSchema.Cols.USERNAME+", "+
+                UserDbSchema.Cols.USERLASTNAME+", "+
+                UserDbSchema.Cols.PHONE+")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}

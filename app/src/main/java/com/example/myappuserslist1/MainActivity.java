e699977180f9520b83dcbef02e3c1d36be65bbea
saprykin-alex <figulkin@gmail.com>
2021-09-01 18:10:14 +0300

    Вывод множества элементов recyclerView из списка пользователей + комментарии

--- app/src/main/java/com/example/myappuserslist1/MainActivity.java
+++ app/src/main/java/com/example/myappuserslist1/MainActivity.java
@@ -2,12 +2,10 @@ package com.example.myappuserslist1;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
